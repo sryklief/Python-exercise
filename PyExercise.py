4fc1f6b148ce42378890f5fcee0bc0ec6017b1fa
sryklief <61458785+sryklief@users.noreply.github.com>
2020-03-10 15:00:31 +0200

    exercise using lists

---
+++ PyExercise.py
@@ -0,0 +1,17 @@
+strSentance = "Lifechoices Coding Academy"
+print(type(strSentance))
+print(strSentance[2:6])
+number1=25
+number2=50.5
+number3=3e+7j
+print(type(number1),type(2))
+#this declares a list called mylist
+mylist=[65, 25, 125, 78, 120, 33]
+mylist.sort()
+print(mylist)
+mylist.insert(2,37)
+print(mylist)
+mylist.sort(reverse=True)
+print(mylist)
+xdict={"1":"Amy","2":"Tiana","3":"Nathan"}
+print(type(xdict))
