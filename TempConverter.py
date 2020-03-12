182c118f7694923d2d2821d19f8fa93aa0deba12
sryklief <61458785+sryklief@users.noreply.github.com>
2020-03-12 21:00:10 +0200

    Converting from celsius to fahrnheit

---
+++ TempConverter.py
@@ -0,0 +1,7 @@
+fahrenheit = int(input("input fahrenheit value to convert to celsius"))
+celsius = (5/9)* (fahrenheit -32)
+print(celsius)
+
+celsius = int(input("input celsius value to convert to fahrenheit"))
+fahrenheit = (celsius * 9/5) +32
+print(fahrenheit)
